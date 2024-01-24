a8d6d5898b8964effa24e73831cb3f13be42cb1c
José <joseevertonmotarodrigues@gmail.com>
2024-01-23 23:22:09 -0300

    Add files via upload

---
+++ numeros.rb
@@ -0,0 +1,14 @@
+puts "insira um número"
+x = gets.chomp.to_i
+puts "insira outro número"
+y = gets.chomp.to_i
+
+soma = x + y
+subt = x - y
+mult = x * y
+div = x.to_f/ y.to_f
+
+puts soma
+puts subt
+puts mult
+puts div
